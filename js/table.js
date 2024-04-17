050e797665838c9f31aecf96df04535b2fe7c2f1
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-04-17 12:00:38 +0200

    refactor table.ts

--- js/table.js
+++ js/table.js
@@ -1,45 +1,50 @@
-const fillEmptyCells = (table) => {
-    const NBSP = "\xa0";
-    getAllTableDataCells(table).forEach(cell => { if (cell.innerText === "")
-        cell.innerText = NBSP; });
-};
-const loadTableStyles = (table) => {
-    const cells = [...getAllTableDataCells(table), ...getAllTableHeaderCells(table)];
-    cells.forEach(cell => {
-        const textAlign = cell.dataset.textAlign;
-        if (textAlign)
-            cell.setAttribute("class", "text-align-" + textAlign);
-    });
-};
+class Table {
+    constructor(table) {
+        this.table = table;
+        const cells = [...this.getAllTableDataCells(), ...this.getAllTableHeaderCells()];
+        cells.forEach(cell => this.getStyleAttributesAndRewrite(cell));
+        const tableHeaders = this.table.querySelectorAll("th");
+        const tableHeaderTexts = this.getAllTextFromTableHeaders();
+        const tableRows = this.table.querySelectorAll("tr");
+        tableRows.forEach(tr => tr.querySelectorAll("td").forEach((td, i) => td.setAttribute("data-label", tableHeaderTexts[i])));
+        this.loadTableStyles();
+        this.fillEmptyCells();
+    }
+    getAllTableDataCells() {
+        return this.table.querySelectorAll("td");
+    }
+    getAllTableHeaderCells() {
+        return this.table.querySelectorAll("th");
+    }
+    fillEmptyCells() {
+        const NBSP = "\xa0";
+        this.getAllTableDataCells().forEach(cell => { if (cell.innerText === "")
+            cell.innerText = NBSP; });
+    }
+    loadTableStyles() {
+        const cells = [...this.getAllTableDataCells(), ...this.getAllTableHeaderCells()];
+        cells.forEach(cell => {
+            const textAlign = cell.dataset.textAlign;
+            if (textAlign)
+                cell.setAttribute("class", "text-align-" + textAlign);
+        });
+    }
+    getAllTextFromTableHeaders() {
+        const tableHeaderTextArr = [];
+        this.getAllTableHeaderCells().forEach(th => tableHeaderTextArr.push(th.innerText));
+        return tableHeaderTextArr;
+    }
+    getStyleAttributesAndRewrite(cell) {
+        const styleAttr = cell.getAttribute("style").split(": ")[1].split(";")[0];
+        if (styleAttr)
+            cell.setAttribute("data-text-align", styleAttr);
+        cell.setAttribute("style", "");
+    }
+}
 const addResponsiveToAllTables = () => {
     const tables = document.querySelectorAll(".table-responsive table");
     tables.forEach(table => {
-        const cells = [
-            ...getAllTableDataCells(table),
-            ...getAllTableHeaderCells(table),
-        ];
-        cells.forEach(cell => getStyleAttributesAndRewrite(cell));
-        const tableHeaders = table.querySelectorAll("th");
-        const tableHeaderTexts = getAllTextFromTableHeaders(tableHeaders);
-        const tableRows = table.querySelectorAll("tr");
-        tableRows
-            .forEach(tr => tr.querySelectorAll("td")
-            .forEach((td, i) => td.setAttribute("data-label", tableHeaderTexts[i])));
-        loadTableStyles(table);
-        fillEmptyCells(table);
+        new Table(table);
     });
 };
-const getAllTextFromTableHeaders = (tableHeaderArr) => {
-    const tableHeaderTextArr = [];
-    tableHeaderArr.forEach(th => tableHeaderTextArr.push(th.innerText));
-    return tableHeaderTextArr;
-};
-const getStyleAttributesAndRewrite = (cell) => {
-    const styleAttr = cell.getAttribute("style").split(": ")[1].split(";")[0];
-    if (styleAttr)
-        cell.setAttribute("data-text-align", styleAttr);
-    cell.setAttribute("style", "");
-};
-const getAllTableDataCells = (table) => table.querySelectorAll("td");
-const getAllTableHeaderCells = (table) => table.querySelectorAll("th");
 addResponsiveToAllTables();
--- ts/table.ts
+++ ts/table.ts
@@ -1,50 +1,62 @@
-const getAllTableDataCells = (table: HTMLTableElement): NodeListOf<HTMLTableCellElement> => table.querySelectorAll("td");
-const getAllTableHeaderCells = (table: HTMLTableElement): NodeListOf<HTMLTableCellElement> => table.querySelectorAll("th");
+class Table {
 
-const fillEmptyCells = (table: HTMLTableElement) => {
-	const NBSP = "\xa0";
-	getAllTableDataCells(table).forEach(cell => { if (cell.innerText === "") cell.innerText = NBSP; });
-};
+	private table: HTMLTableElement;
 
-const loadTableStyles = (table: HTMLTableElement): void => {
-	const cells = [...getAllTableDataCells(table), ...getAllTableHeaderCells(table)];
-	cells.forEach(cell => {
-		const textAlign = cell.dataset.textAlign;
-		if (textAlign) cell.setAttribute("class", "text-align-" + textAlign);
-	});
-};
+	constructor(table: HTMLTableElement) {
+		this.table = table;
 
-const getAllTextFromTableHeaders = (tableHeaderArr: NodeListOf<HTMLTableCellElement>): string[] => {
-	const tableHeaderTextArr = [];
-	tableHeaderArr.forEach(th => tableHeaderTextArr.push(th.innerText));
-	return tableHeaderTextArr;
-};
+		const cells = [...this.getAllTableDataCells(), ...this.getAllTableHeaderCells()];
+		cells.forEach(cell => this.getStyleAttributesAndRewrite(cell));
 
-const getStyleAttributesAndRewrite = (cell: HTMLTableCellElement): void => {
-	const styleAttr = cell.getAttribute("style").split(": ")[1].split(";")[0];
-	if (styleAttr) cell.setAttribute("data-text-align", styleAttr);
-	cell.setAttribute("style", "");
-};
+		const tableHeaders = this.table.querySelectorAll("th");
+		const tableHeaderTexts = this.getAllTextFromTableHeaders();
+
+		const tableRows = this.table.querySelectorAll("tr");
+		tableRows.forEach(tr => tr.querySelectorAll("td").forEach((td, i) => td.setAttribute("data-label", tableHeaderTexts[i])));
+
+		this.loadTableStyles();
+		this.fillEmptyCells();
+	}
+
+	private getAllTableDataCells() {
+		return this.table.querySelectorAll("td");
+	}
+
+	private getAllTableHeaderCells() {
+		return this.table.querySelectorAll("th");
+	}
+
+	private fillEmptyCells() {
+		const NBSP = "\xa0";
+		this.getAllTableDataCells().forEach(cell => { if (cell.innerText === "") cell.innerText = NBSP; });
+	}
+
+	private loadTableStyles() {
+		const cells = [...this.getAllTableDataCells(), ...this.getAllTableHeaderCells()];
+		cells.forEach(cell => {
+			const textAlign = cell.dataset.textAlign;
+			if (textAlign) cell.setAttribute("class", "text-align-" + textAlign);
+		});
+	}
+
+	private getAllTextFromTableHeaders() {
+		const tableHeaderTextArr = [];
+		this.getAllTableHeaderCells().forEach(th => tableHeaderTextArr.push(th.innerText));
+		return tableHeaderTextArr;
+	}
+
+	private getStyleAttributesAndRewrite(cell: HTMLTableCellElement) {
+		const styleAttr = cell.getAttribute("style").split(": ")[1].split(";")[0];
+		if (styleAttr) cell.setAttribute("data-text-align", styleAttr);
+		cell.setAttribute("style", "");
+	}
+
+}
 
 const addResponsiveToAllTables = (): void => {
 	const tables = document.querySelectorAll(".table-responsive table") as NodeListOf<HTMLTableElement>;
 	tables.forEach(table => {
-		const cells = [
-			...getAllTableDataCells(table),
-			...getAllTableHeaderCells(table),
-		];
-		cells.forEach(cell => getStyleAttributesAndRewrite(cell));
-
-		const tableHeaders = table.querySelectorAll("th");
-		const tableHeaderTexts = getAllTextFromTableHeaders(tableHeaders);
-
-		const tableRows = table.querySelectorAll("tr");
-		tableRows
-			.forEach(tr => tr.querySelectorAll("td")
-				.forEach((td, i) => td.setAttribute("data-label", tableHeaderTexts[i])));
-
-		loadTableStyles(table);
-		fillEmptyCells(table);
+		new Table(table);
 	});
 };
 
