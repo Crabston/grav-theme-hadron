5f983f5056940ded2ff5ec117e1c3bcc840ca439
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-04-20 15:51:44 +0200

    extend base template instead of replacing it

--- minimal.php
+++ minimal.php
@@ -1,9 +1,23 @@
 <?php
 namespace Grav\Theme;
 
+use Grav\Common\Grav;
 use Grav\Common\Theme;
 
 class Minimal extends QuarkOpenPublishing
 {
-    // Access plugin events in this class
+	public static function getSubscribedEvents() {
+		return [
+			'onTwigLoader' => ['onTwigLoader', 10]
+		];
+	}
+
+	public function onTwigLoader() {
+		parent::onTwigLoader();
+
+		// add parent theme as namespace to twig
+		$parentThemeName = 'quark-open-publishing';
+		$parentThemePath = Grav::instance()['locator']->findResource('themes://' . $parentThemeName);
+		$this->grav['twig']->addPath($parentThemePath . DIRECTORY_SEPARATOR . 'templates', $parentThemeName);
+	}
 }
