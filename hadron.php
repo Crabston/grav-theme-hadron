e8874eb4e0757f149da9dd48d420efbd95556b44
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-09-26 14:48:37 +0200

    theme: update php init code

--- hadron.php
+++ hadron.php
@@ -4,22 +4,69 @@ namespace Grav\Theme;
 use Grav\Common\Grav;
 use Grav\Common\Theme;
 
-// TODO: extend Quark theme instead of QuarkOpenPublishing
-class Hadron extends QuarkOpenPublishing
-{
+class Hadron extends Quark {
 	public static function getSubscribedEvents() {
 		return [
-			'onTwigLoader' => ['onTwigLoader', 10]
+			'onTwigLoader' => ['onTwigLoader', 0],
+			'onShortcodeHandlers' => ['onShortcodeHandlers', 0],
+			'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+			'onTwigInitialized' => ['onTwigInitialized', 0],
+			'registerNextGenEditorPlugin' => ['registerNextGenEditorPluginShortcodes', 0]
 		];
+	}
+
+	public function onTwigInitialized() {
+		$twig = $this->grav['twig'];
+
+		$form_class_variables = [
+			//'form_outer_classes' => 'form-horizontal',
+			'form_button_outer_classes' => 'button-wrapper',
+			'form_button_classes' => 'btn',
+			'form_errors_classes' => '',
+			'form_field_outer_classes' => 'form-group',
+			'form_field_outer_label_classes' => 'form-label-wrapper',
+			'form_field_label_classes' => 'form-label',
+			//'form_field_outer_data_classes' => 'col-9',
+			'form_field_input_classes' => 'form-input',
+			'form_field_textarea_classes' => 'form-input',
+			'form_field_select_classes' => 'form-select',
+			'form_field_radio_classes' => 'form-radio',
+			'form_field_checkbox_classes' => 'form-checkbox',
+		];
+
+		$twig->twig_vars = array_merge($twig->twig_vars, $form_class_variables);
 	}
 
 	public function onTwigLoader() {
 		parent::onTwigLoader();
 
 		// add parent theme as namespace to twig
-		// TODO: extend Quark theme instead of QuarkOpenPublishing
-		$parentThemeName = 'quark-open-publishing';
+		$parentThemeName = 'quark';
 		$parentThemePath = Grav::instance()['locator']->findResource('themes://' . $parentThemeName);
 		$this->grav['twig']->addPath($parentThemePath . DIRECTORY_SEPARATOR . 'templates', $parentThemeName);
+	}
+
+	public static function getblogpageheroclasses()	{
+		$config = Grav::instance()['config'];
+		return $config->get('themes.' . $config->get('system.pages.theme') . '.blog_page_hero_classes');
+	}
+
+	public function registerNextGenEditorPluginShortcodes($event) {
+		$plugins = $event['plugins'];
+
+		$plugins['js'][] = 'user://themes/quark-open-publishing/nextgen-editor/shortcodes/h5p.js';
+
+		$event['plugins']  = $plugins;
+		return $event;
+	}
+
+	public function onShortcodeHandlers() {
+		$this->grav['shortcode']->registerAllShortcodes('user://themes/hadron/shortcodes');
+	}
+
+	public function onTwigSiteVariables() {
+		if ($this->isAdmin() && ($this->grav['config']->get('plugins.shortcode-core.enabled'))) {
+			$this->grav['assets']->add('theme://editor-buttons/admin/js/shortcode-h5p.js');
+		}
 	}
 }
