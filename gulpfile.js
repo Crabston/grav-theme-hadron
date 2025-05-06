6abd505292763f9dc278cc047f4ebcec0d5c5d4a
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2025-05-06 18:15:48 +0200

    gulp(buildStyles): task to compile scss to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
-function defaultTask(cb) {
-	// place code for your default task here
-	cb();
-}
+"use strict";
 
-exports.default = defaultTask
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+
+function buildStyles() {
+	return gulp.src("./scss/**/*.scss")
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(sass().on("error", sass.logError))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest("./css"));
+};
+
+exports.buildStyles = buildStyles;
+exports.watch = function() {
+	gulp.watch("./scss/**/*.scss", buildStyles);
+};
