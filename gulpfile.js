6446644ab5833723e9e222049dd7c75953286227
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2025-05-06 18:29:08 +0200

    gulp(minifyCss): task to minify css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
+const cleanCSS = require("gulp-clean-css");
 
 function buildStyles() {
 	return gulp.src("./scss/**/*.scss")
@@ -12,7 +13,17 @@ function buildStyles() {
 		.pipe(gulp.dest("./css"));
 };
 
+function minifyCss() {
+	return gulp.src("./css/**/*.css")
+		.pipe(cleanCSS({ debug: true, level: 0 }, (details) => {
+			console.log(`Before: ${details.name}: ${details.stats.originalSize}`);
+			console.log(`After_: ${details.name}: ${details.stats.minifiedSize}`);
+		}))
+		.pipe(gulp.dest("./css"));
+}
+
 exports.buildStyles = buildStyles;
+exports.minifyCss = minifyCss;
 exports.watch = function() {
 	gulp.watch("./scss/**/*.scss", buildStyles);
 };
