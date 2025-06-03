73688d5c0a134a28621d44a2d62a211607e9b2cb
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2025-06-03 14:52:24 +0200

    #26 Optimize Hadron

--- js/hadron.js
+++ js/hadron.js
@@ -1,72 +1,103 @@
 var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
-
 function scrollHeader() {
-    // Has scrolled class on header
     var zvalue = $(document).scrollTop();
-    if ( zvalue > 75 )
+    if (zvalue > 75)
         $("#header").addClass("scrolled");
     else
         $("#header").removeClass("scrolled");
 }
-
 function parallaxBackground() {
     $('.parallax').css('background-positionY', ($(window).scrollTop() * 0.3) + 'px');
 }
-
-jQuery(document).ready(function($){
-
+jQuery(document).ready(function ($) {
     scrollHeader();
-
-    // Scroll Events
-    if (!isTouch){
-        $(document).scroll(function() {
+    if (!isTouch) {
+        $(document).scroll(function () {
             scrollHeader();
             parallaxBackground();
         });
-    };
-
-    // Touch scroll
+    }
+    ;
     $(document).on({
-        'touchmove': function(e) {
-            scrollHeader(); // Replace this with your code.
+        'touchmove': function (e) {
+            scrollHeader();
         }
     });
-
-    //Smooth scroll to start
-    $('#to-start').click(function(){
+    $('#to-start').click(function () {
         var start_y = $('#start').position().top;
         var header_offset = 45;
         window.scroll({ top: start_y - header_offset, left: 0, behavior: 'smooth' });
         return false;
     });
-
-    //Smooth scroll to top
-    $('#to-top').click(function(){
+    $('#to-top').click(function () {
         window.scroll({ top: 0, left: 0, behavior: 'smooth' });
         return false;
     });
-
-    // Responsive Menu
     $('#toggle').click(function () {
         $(this).toggleClass('active');
         $('#overlay').toggleClass('open');
         $('body').toggleClass('mobile-nav-open');
     });
-
-    // Tree Menu
-    $(".tree").treemenu({delay:300});
-
+    $(".tree").treemenu({ delay: 300 });
 });
-
-document.querySelectorAll( 'oembed[url]' ).forEach( element => {
-    // Create the <a href="..." class="embedly-card"></a> element that Embedly uses
-    // to discover the media.
-    const anchor = document.createElement( 'a' );
-
-    anchor.setAttribute( 'href', element.getAttribute( 'url' ) );
+document.querySelectorAll('oembed[url]').forEach(element => {
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', element.getAttribute('url'));
     anchor.className = 'embedly-card';
     anchor.dataset.cardAlign = 'left';
     anchor.dataset.cardControls = 0;
-
-    element.appendChild( anchor );
-} );
+    element.appendChild(anchor);
+});
+class ResponsiveTable {
+    constructor(table) {
+        this.table = table;
+        const cells = [
+            ...this.getDataCells(),
+            ...this.getHeaderCells(),
+        ];
+        cells.forEach(cell => this.rewriteStyleAttribute(cell));
+        this.getRows()
+            .forEach(tr => tr.querySelectorAll("td")
+            .forEach((td, i) => td.setAttribute("data-label", (this.getTextFromTableHeaders())[i])));
+        this.loadTableStyles();
+        this.fillEmptyCells();
+    }
+    getRows() {
+        return this.table.querySelectorAll("tr");
+    }
+    getDataCells() {
+        return this.table.querySelectorAll("td");
+    }
+    getHeaderCells() {
+        return this.table.querySelectorAll("th");
+    }
+    getTextFromTableHeaders() {
+        const tableHeaderTextArr = [];
+        this.getHeaderCells().forEach(th => tableHeaderTextArr.push(th.innerText));
+        return tableHeaderTextArr;
+    }
+    rewriteStyleAttribute(cell) {
+        const styleAttr = cell.getAttribute("style").split(": ")[1].split(";")[0];
+        if (styleAttr)
+            cell.setAttribute("data-text-align", styleAttr);
+        cell.setAttribute("style", "");
+    }
+    loadTableStyles() {
+        const cells = [...this.getDataCells(), ...this.getHeaderCells()];
+        cells.forEach(cell => {
+            const textAlign = cell.dataset.textAlign;
+            if (textAlign)
+                cell.setAttribute("class", "text-align-" + textAlign);
+        });
+    }
+    fillEmptyCells() {
+        const NBSP = "\xa0";
+        this.getDataCells().forEach(cell => { if (cell.innerText === "")
+            cell.innerText = NBSP; });
+    }
+}
+(function makeTablesResponsive() {
+    const selector = ".table-responsive table";
+    const tables = document.querySelectorAll(selector);
+    tables.forEach(table => new ResponsiveTable(table));
+})();
